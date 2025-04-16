565f1125f89132e0f6c14477ffc996229e8e93bb
zingzzi <pangdoa.merry@gamil.com>
2025-04-16 16:30:31 +0900

    db 리스트 페이지 생성성

---
+++ app/list/page.js
@@ -0,0 +1,25 @@
+import { connectDB } from "/util/database.js"
+export default async function List(){
+    
+  let client = await connectDB;
+  const db = client.db('forum');
+  let result = await db.collection('post').find().toArray();
+    return(
+        <div className="list-bg">
+            <div className="list-item">
+                {
+                    result.map((e,i)=>{
+                        return(
+                        <div  className="list-item" key={i}>
+                            <h4>{e.title}</h4>
+                            <p>{e.content}</p>
+                        </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+
+        
+    )
+}
